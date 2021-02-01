f33adc259922a7fd0f2e88c2182a03cc8b56f0a8
Pavel <sidorovich.pasha@gmail.com>
2021-02-02 02:25:30 +0300

    first pool

---
+++ src/minecraft++.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+#include <tinyGL.h>
+
+int main()
+{
+    tgl::View window(640, 480, L"Minecraft++");
+
+    for (;window.is_open();)
+    {
+        tgl::event_pool();   
+    }
+
+    return 0;
+}
