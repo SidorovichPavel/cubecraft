1436a894172c311f40090bb616fce5a42656cda1
Pavel <sidorovich.pasha@gmail.com>
2021-02-03 00:41:35 +0300

    draw white triangle)

--- src/minecraft++.cpp
+++ src/minecraft++.cpp
@@ -1,18 +1,34 @@
 ﻿#include <iostream>
 
-#include <tinyGL.h>
-
-#include <gl/GL.h>
+#include <src/tinyGL.h>
 
 int main()
 {
+    tgl::Init();
+
     tgl::View window(640, 480, L"Minecraft++");
+    
+    std::array<float, 9> triangle{
+        0.f, .5f, 0.f,
+        -.5f, -.5f, 0.f,
+        .5f, -.5f, 0.f
+    };
+
+    tgl::VAO vao;
+    vao.push_vbo(triangle);
+
+    tgl::Shader first("first");
+    first.bind_attribute(0, "position");
+    first.link();
 
     for (;window.is_open();)
     {
         tgl::event_pool();
-        glClear(GL_COLOR_BUFFER_BIT);
-        glClearColor(0.65, 0.4, 0.15, 1);
+        tgl::gl::glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        tgl::gl::glClearColor(0.75f, 0.5f, 0.35f, 1.f);
+        
+        vao.draw(3);
+
         window.swap_buffers();
     }
 
