563431f5ee965fe2c717468b3aef7aaf20917d54
Pavel <sidorovich.pasha@gmail.com>
2021-02-03 01:30:06 +0300

    added colored shaders

--- src/minecraft++.cpp
+++ src/minecraft++.cpp
@@ -21,11 +21,14 @@ int main()
     first.bind_attribute(0, "position");
     first.link();
 
+    first.use();
+
     for (;window.is_open();)
     {
         tgl::event_pool();
         tgl::gl::glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         tgl::gl::glClearColor(0.75f, 0.5f, 0.35f, 1.f);
+        
         
         vao.draw(3);
 
