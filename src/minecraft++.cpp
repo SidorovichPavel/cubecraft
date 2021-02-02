1cecc71cfe5c2b37e9016f86ed70e7d6fa70edcd
Pavel <sidorovich.pasha@gmail.com>
2021-02-02 05:36:44 +0300

    add opengl render

--- src/minecraft++.cpp
+++ src/minecraft++.cpp
@@ -2,13 +2,18 @@
 
 #include <tinyGL.h>
 
+#include <gl/GL.h>
+
 int main()
 {
     tgl::View window(640, 480, L"Minecraft++");
 
     for (;window.is_open();)
     {
-        tgl::event_pool();   
+        tgl::event_pool();
+        glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(0.65, 0.4, 0.15, 1);
+        window.swap_buffers();
     }
 
     return 0;
