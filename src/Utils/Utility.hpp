15e3303423fb2d7bf5f919e797c18e2a59b900c6
Pavel <sidorovich.pasha@gmail.com>
2022-03-28 04:57:58 +0300

    ;-)

--- src/Utils/Utility.hpp
+++ src/Utils/Utility.hpp
@@ -1,11 +1,2 @@
 #pragma once
-#include <chrono>
-//#include <>
 
-struct FrameTimeInfo
-{
-	float duration;
-	std::chrono::time_point<std::chrono::steady_clock> timepoint;
-};
-
-#define uniform(name) #name, &##name
