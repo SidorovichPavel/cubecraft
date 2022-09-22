b6a794825527965e7e5ec61bd17180c7b9900c74
Pavel <sidorovich.pasha@gmail.com>
2022-09-22 18:41:43 +0300

    add mouse sensitivity

--- src/MineDevices/MineMouse.cpp
+++ src/MineDevices/MineMouse.cpp
@@ -2,10 +2,22 @@
 
 MineMouse::MineMouse(int32_t _X, int32_t _Y)
 	:
-	base(_X, _Y)
+	base(_X, _Y),
+	mSensitivity(6.f)
 {
 }
 
 MineMouse::~MineMouse() noexcept
 {
+}
+
+void MineMouse::shift(int32_t _LastX, int32_t _LastY) noexcept
+{
+	mUpdated = true;
+
+	lastX = x;
+	lastY = y;
+
+	x += static_cast<int32_t>(_LastX * mSensitivity);
+	y += static_cast<int32_t>(_LastY * mSensitivity);
 }
--- src/MineDevices/MineMouse.hpp
+++ src/MineDevices/MineMouse.hpp
@@ -7,8 +7,12 @@ class MineMouse final :
 {
     using base = tgl::Mouse;
 
+    float mSensitivity;
+
 public:
     MineMouse(int32_t _X = 0, int32_t _Y = 0);
     ~MineMouse() noexcept;
+
+    void shift(int32_t _LastX, int32_t _LastY) noexcept override;
 };
 
